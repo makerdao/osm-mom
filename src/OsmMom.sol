cd530d5aa0a00e8b83391ed9a83804c9dc9b8911
andy8052 <andychorlian@gmail.com>
2019-12-11 09:42:49 -0500

    Initial Changes

--- src/OsmMom.sol
+++ src/OsmMom.sol
@@ -1,4 +1,59 @@
+/// OsmMom -- governance interface for the OSM
+
+// Copyright (C) 2019 Maker Ecosystem Growth Holdings, INC.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 pragma solidity ^0.5.12;
 
-contract OsmMom {
+import "ds-note/note.sol";
+
+interface OsmLike {
+    function stop() external;
+    function start() external;
+    function void() external;
+}
+
+contract OsmMom is DSNote {
+    address public owner;
+    modifier onlyOwner { require(msg.sender == owner); _;}
+
+    mapping (address => uint) public wards;
+    function rely(address usr) public note onlyOwner { wards[usr] = 1; }
+    function deny(address usr) public note onlyOwner { wards[usr] = 0; }
+    modifier auth { require(owner == msg.sender || wards[msg.sender] == 1); _; }
+
+    OsmLike public osm;
+
+    constructor(OsmLike osm_) public {
+        owner = msg.sender;
+        osm = osm_;
+    }
+
+    function setOwner(address owner_) public note onlyOwner {
+        owner = owner_;
+    }
+
+    function stop() public auth {
+        osm.stop();
+    }
+
+    function start() public auth {
+        osm.start();
+    }
+
+    function void() public auth {
+        osm.void();
+    }
 }
--- src/OsmMom.t.sol
+++ src/OsmMom.t.sol
@@ -4,11 +4,46 @@ import "ds-test/test.sol";
 
 import "./OsmMom.sol";
 
+contract OsmMomCaller {
+  OsmMom mom;
+
+  constructor(OsmMom mom_) public {
+    mom = mom_;
+  }
+
+  function setOwner(address newOwner) public {
+    mom.setOwner(newOwner);
+  }
+
+  function rely(address usr) public {
+    mom.rely(usr);
+  }
+
+  function deny(address usr) public {
+    mom.deny(usr);
+  }
+
+  function stop() public {
+    mom.stop();
+  }
+
+  function start() public {
+    mom.start();
+  }
+
+  function void() public {
+    mom.void();
+  }
+
+}
+
 contract OsmMomTest is DSTest {
+    OsmLike osm;
     OsmMom mom;
+    OsmMomCaller caller;
 
     function setUp() public {
-        mom = new OsmMom();
+        // mom = new OsmMom();
     }
 
     function testFail_basic_sanity() public {
